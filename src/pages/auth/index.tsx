769b5c1658befca01c873aa5d4e566701a13bb27
Massaki <alisonmassaki@gmail.com>
2024-10-27 16:15:33 -0300

    Instala recoil e cria os modais de autenticação

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,19 +1,24 @@
-
-import Navbar from "@/components/NavBar/Navbar";
+import { authModalState } from "@/atoms/authModalAtom";
+import AuthModal from "@/components/Modals/AuthModal";
+import { useRecoilValue } from "recoil";
 import Image from "next/image";
+import Navbar from "@/components/NavBar/Navbar";
 
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = () => {
-	return (
-		<div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
-			<div className='max-w-7xl mx-auto'>
-				<Navbar />
-				<div className='flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none'>
-					<Image src='/hero.png' alt='Hero img' width={700} height={700} />
-				</div>
-			</div>
-		</div>
-	);
+  const authModal = useRecoilValue(authModalState);
+
+  return (
+    <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
+      <div className="max-w-7xl mx-auto">
+        <Navbar />
+        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+          <Image src="/hero.png" alt="Hero img" width={700} height={700} />
+        </div>
+        {authModal.isOpen && <AuthModal />}
+      </div>
+    </div>
+  );
 };
 export default AuthPage;
