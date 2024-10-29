4ef0fb55aeb1dd188cbe3afadbd804f253712d9f
alisonmsk <alisonmsk@gmail.com>
2024-10-29 15:05:23 -0300

    Define título e favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,20 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Head>
+        <title>LeetClone</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+        <meta
+          name="description"
+          content="Clone do site leetcode desenvolvido através do curso publico no Youtube por freeCodeCamp.org"
+        />
+      </Head>
       <ToastContainer />
       <Component {...pageProps} />
     </RecoilRoot>
